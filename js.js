0ad4d7dda0b41744a16f693bde166a9a0e3d60f5
L1nkin <55152868+L1nkin@users.noreply.github.com>
2019-11-13 00:04:11 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ function epol()
 {
     let arr1 = document.getElementById('firstArray').value.split(' ').splice(0, 8);
     let arr2 = document.getElementById('secondArray').value.split(' ').splice(0, 8);
-    let newArray = arr1.map(function(value) {
+    let newArray = arr1.map(function(value) { 
         if (arr2.indexOf(value) == -1)
             value = 0;
         else
